4a68d1dc3266bd92b5a0256d34829dcd27016709
Diego Puertolas <diegopuertolasruiz@gmail.com>
2026-02-14 12:08:42 +0100

    Add update route for videogames with validation

--- src/router/videogamesRouter.js
+++ src/router/videogamesRouter.js
@@ -3,12 +3,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllVideogames, getVideogameById, postVideogame } = require('../controller/videogamesController');
-const { validateVideogameId, validateAddVideogame } = require('../validators/videogames');
+const { getAllVideogames, getVideogameById, postVideogame, putVideogame } = require('../controller/videogamesController');
+const { validateVideogameId, validateAddVideogame, validateUpdateVideogame } = require('../validators/videogames');
 
 // RUTAS
 router.get('/', getAllVideogames);
 router.get('/:id',validateVideogameId, getVideogameById );
 router.post('/', validateAddVideogame, postVideogame);
+router.put('/:id', validateUpdateVideogame, putVideogame);
 
 module.exports = router;
